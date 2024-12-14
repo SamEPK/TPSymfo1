204d897bab06ec8d26a2b660332ec00ea2532d17
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2024-12-14 16:45:34 +0100

    films entitys

---
+++ migrations/Version20241214154455.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241214154455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE movie_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE serie_id_seq CASCADE');
+        $this->addSql('CREATE TABLE media_category (media_id INT NOT NULL, category_id INT NOT NULL, PRIMARY KEY(media_id, category_id))');
+        $this->addSql('CREATE INDEX IDX_92D3773EA9FDD75 ON media_category (media_id)');
+        $this->addSql('CREATE INDEX IDX_92D377312469DE2 ON media_category (category_id)');
+        $this->addSql('CREATE TABLE media_language (media_id INT NOT NULL, language_id INT NOT NULL, PRIMARY KEY(media_id, language_id))');
+        $this->addSql('CREATE INDEX IDX_DBBA5F07EA9FDD75 ON media_language (media_id)');
+        $this->addSql('CREATE INDEX IDX_DBBA5F0782F1BAF4 ON media_language (language_id)');
+        $this->addSql('ALTER TABLE media_category ADD CONSTRAINT FK_92D3773EA9FDD75 FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE media_category ADD CONSTRAINT FK_92D377312469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE media_language ADD CONSTRAINT FK_DBBA5F07EA9FDD75 FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE media_language ADD CONSTRAINT FK_DBBA5F0782F1BAF4 FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE media ALTER release_date TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
+        $this->addSql('ALTER TABLE media RENAME COLUMN media_type TO mediaType');
+        $this->addSql('COMMENT ON COLUMN media.release_date IS NULL');
+        $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1D5EF26FBF396750 FOREIGN KEY (id) REFERENCES media (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE playlist ADD creator_id INT NOT NULL');
+        $this->addSql('ALTER TABLE playlist ADD CONSTRAINT FK_D782112D61220EA6 FOREIGN KEY (creator_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_D782112D61220EA6 ON playlist (creator_id)');
+        $this->addSql('ALTER TABLE playlist_media ADD playlist_id INT NOT NULL');
+        $this->addSql('ALTER TABLE playlist_media ADD media_id INT NOT NULL');
+        $this->addSql('ALTER TABLE playlist_media ADD CONSTRAINT FK_C930B84F6BBD148 FOREIGN KEY (playlist_id) REFERENCES playlist (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE playlist_media ADD CONSTRAINT FK_C930B84FEA9FDD75 FOREIGN KEY (media_id) REFERENCES media (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_C930B84F6BBD148 ON playlist_media (playlist_id)');
+        $this->addSql('CREATE INDEX IDX_C930B84FEA9FDD75 ON playlist_media (media_id)');
+        $this->addSql('ALTER TABLE playlist_subscription ADD playlist_id INT NOT NULL');
+        $this->addSql('ALTER TABLE playlist_subscription ADD subscriber_id INT NOT NULL');
+        $this->addSql('ALTER TABLE playlist_subscription ADD CONSTRAINT FK_832940C6BBD148 FOREIGN KEY (playlist_id) REFERENCES playlist (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE playlist_subscription ADD CONSTRAINT FK_832940C7808B1AD FOREIGN KEY (subscriber_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_832940C6BBD148 ON playlist_subscription (playlist_id)');
+        $this->addSql('CREATE INDEX IDX_832940C7808B1AD ON playlist_subscription (subscriber_id)');
+        $this->addSql('ALTER TABLE season DROP CONSTRAINT fk_f0e45ba9b748aac3');
+        $this->addSql('DROP INDEX idx_f0e45ba9b748aac3');
+        $this->addSql('ALTER TABLE season ADD number VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE season DROP serie_id_id');
+        $this->addSql('ALTER TABLE season RENAME COLUMN season_number TO serie_id');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA9D94388BD FOREIGN KEY (serie_id) REFERENCES serie (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_F0E45BA9D94388BD ON season (serie_id)');
+        $this->addSql('ALTER TABLE serie ADD CONSTRAINT FK_AA3A9334BF396750 FOREIGN KEY (id) REFERENCES media (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE subscription_history ADD subscriber_id INT NOT NULL');
+        $this->addSql('ALTER TABLE subscription_history ADD subscription_id INT NOT NULL');
+        $this->addSql('ALTER TABLE subscription_history ADD CONSTRAINT FK_54AF90D07808B1AD FOREIGN KEY (subscriber_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE subscription_history ADD CONSTRAINT FK_54AF90D09A1887DC FOREIGN KEY (subscription_id) REFERENCES subscription (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_54AF90D07808B1AD ON subscription_history (subscriber_id)');
+        $this->addSql('CREATE INDEX IDX_54AF90D09A1887DC ON subscription_history (subscription_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE movie_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE serie_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('ALTER TABLE media_category DROP CONSTRAINT FK_92D3773EA9FDD75');
+        $this->addSql('ALTER TABLE media_category DROP CONSTRAINT FK_92D377312469DE2');
+        $this->addSql('ALTER TABLE media_language DROP CONSTRAINT FK_DBBA5F07EA9FDD75');
+        $this->addSql('ALTER TABLE media_language DROP CONSTRAINT FK_DBBA5F0782F1BAF4');
+        $this->addSql('DROP TABLE media_category');
+        $this->addSql('DROP TABLE media_language');
+        $this->addSql('ALTER TABLE media ALTER release_date TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
+        $this->addSql('ALTER TABLE media RENAME COLUMN mediaType TO media_type');
+        $this->addSql('COMMENT ON COLUMN media.release_date IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE serie DROP CONSTRAINT FK_AA3A9334BF396750');
+        $this->addSql('ALTER TABLE movie DROP CONSTRAINT FK_1D5EF26FBF396750');
+        $this->addSql('ALTER TABLE playlist DROP CONSTRAINT FK_D782112D61220EA6');
+        $this->addSql('DROP INDEX IDX_D782112D61220EA6');
+        $this->addSql('ALTER TABLE playlist DROP creator_id');
+        $this->addSql('ALTER TABLE season DROP CONSTRAINT FK_F0E45BA9D94388BD');
+        $this->addSql('DROP INDEX IDX_F0E45BA9D94388BD');
+        $this->addSql('ALTER TABLE season ADD serie_id_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE season DROP number');
+        $this->addSql('ALTER TABLE season RENAME COLUMN serie_id TO season_number');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT fk_f0e45ba9b748aac3 FOREIGN KEY (serie_id_id) REFERENCES serie (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX idx_f0e45ba9b748aac3 ON season (serie_id_id)');
+        $this->addSql('ALTER TABLE playlist_media DROP CONSTRAINT FK_C930B84F6BBD148');
+        $this->addSql('ALTER TABLE playlist_media DROP CONSTRAINT FK_C930B84FEA9FDD75');
+        $this->addSql('DROP INDEX IDX_C930B84F6BBD148');
+        $this->addSql('DROP INDEX IDX_C930B84FEA9FDD75');
+        $this->addSql('ALTER TABLE playlist_media DROP playlist_id');
+        $this->addSql('ALTER TABLE playlist_media DROP media_id');
+        $this->addSql('ALTER TABLE playlist_subscription DROP CONSTRAINT FK_832940C6BBD148');
+        $this->addSql('ALTER TABLE playlist_subscription DROP CONSTRAINT FK_832940C7808B1AD');
+        $this->addSql('DROP INDEX IDX_832940C6BBD148');
+        $this->addSql('DROP INDEX IDX_832940C7808B1AD');
+        $this->addSql('ALTER TABLE playlist_subscription DROP playlist_id');
+        $this->addSql('ALTER TABLE playlist_subscription DROP subscriber_id');
+        $this->addSql('ALTER TABLE subscription_history DROP CONSTRAINT FK_54AF90D07808B1AD');
+        $this->addSql('ALTER TABLE subscription_history DROP CONSTRAINT FK_54AF90D09A1887DC');
+        $this->addSql('DROP INDEX IDX_54AF90D07808B1AD');
+        $this->addSql('DROP INDEX IDX_54AF90D09A1887DC');
+        $this->addSql('ALTER TABLE subscription_history DROP subscriber_id');
+        $this->addSql('ALTER TABLE subscription_history DROP subscription_id');
+    }
+}
